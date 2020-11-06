a9680c618e4dcdcd93f2567d7b173b017a9a4f13
Boaventura <scavenger.gnu@gmail.com>
2020-11-05 21:51:35 -0300

    :construction: post parser and tests.

---
+++ lib/src/core/parser/post_parser.dart
@@ -0,0 +1,151 @@
+import '../model/post.dart';
+import 'IHtmlParser.dart';
+import 'package:html/parser.dart' as htmlParser show parse;
+import 'package:html/dom.dart';
+
+/// This class is reposible for hanle a html page with posts
+/// and extract all the posts from that page.
+/// Pages like home page and category page should use this class
+/// to parse the posts.
+class PostParser implements IHtmlParser<List<Post>> {
+  @override
+  List<Post> parse(String html) {
+    final document = htmlParser.parse(
+      html,
+    );
+
+    final loopBlog = document.getElementById('loopBlog');
+    return _postsExtractor(loopBlog);
+  }
+
+  List<Post> _postsExtractor(final Element loopBlogElement) {
+    final articles = loopBlogElement?.getElementsByTagName('article');
+
+    return articles.map((element) {
+      try {
+        return _postExtractor(element);
+      } catch (_) {
+        print(
+            "Was not possible parse an element in PostParser::postsExtractor $_");
+      }
+    }).toList();
+  }
+
+  Post _postExtractor(Element element) {
+    // getting post category name
+    final categoryName = _extractCategoryName(element);
+
+    // getting post Image url
+    final imageUrl = _extractImageUrl(element);
+
+    // getting post title
+    final title = _extractTitle(element);
+
+    // getting post author
+    final author = _extractAuthor(element);
+
+    // getting post url
+    final contentUrl = _extractPostContentUrl(element);
+
+    return Post(
+      author: author,
+      categoryName: categoryName,
+      imageUrl: imageUrl,
+      contentUrl: contentUrl,
+      title: title,
+    );
+  }
+
+  String _extractCategoryName(Element element) {
+    try {
+      final categoryAnchor = element
+          ?.getElementsByTagName('ul')
+          ?.first
+          ?.getElementsByTagName('li')
+          ?.first
+          ?.getElementsByTagName('a')
+          ?.first;
+      return categoryAnchor?.text;
+    } catch (_) {
+      print("PostParser::_extractCategoryName couldn't find category name");
+    }
+    return "";
+  }
+
+  String _extractImageUrl(Element element) {
+    try {
+      final thumbAnchor = element
+          ?.getElementsByClassName("thumb")
+          ?.first
+          ?.getElementsByTagName('a')
+          ?.first;
+      final img = thumbAnchor?.getElementsByTagName('img')?.first;
+      return img?.attributes["src"] ?? null;
+    } catch (ex) {
+      print("PostParser::_extractImageUrl couldn't find image url");
+    }
+
+    return null;
+  }
+
+  String _extractTitle(Element element) {
+    try {
+      final titleAnchor = element
+          ?.getElementsByClassName('conteudoPost')
+          ?.first
+          ?.getElementsByTagName('a')
+          ?.first;
+      return titleAnchor?.text ?? "";
+    } catch (ex) {
+      print("PostParser::_extractTitle couldn't find title");
+    }
+    return "";
+  }
+
+  String _extractAuthor(Element element) {
+    try {
+      return element
+          ?.getElementsByClassName("authorPost")
+          ?.first
+          ?.getElementsByTagName('p')
+          ?.first
+          ?.text;
+    } catch (ex) {
+      print("PostParser::_extractAuthor couldn't find author");
+    }
+    return "";
+  }
+
+  String _extractPostContentUrl(Element element) {
+    try {
+      return element
+          ?.getElementsByClassName("conteudoPost")
+          ?.first
+          ?.getElementsByTagName('a')
+          ?.first
+          ?.attributes["href"];
+    } catch (ex) {
+      print("PostParser::_extractPostContentUrl couldn't find content url");
+    }
+    return null;
+  }
+// <div class="categoryPost" id="space-today-tv">
+//  <ul class="post-categories">
+//    <li><a href="https://spacetoday.com.br/category/space-today-tv/" rel="category tag">Space Today TV</a></li></ul>
+// </div>
+//
+//  <div class="thumb">
+//    <a href="https://spacetoday.com.br/atualizacoes-da-osiris-rex-guardando-as-amostras-do-asteroide-bennu-ao-vivo/"><img width="301" height="316" src="https://spacetoday.com.br/wp-content/uploads/2020/10/D4FUalrTkgU-301x316.jpg" class="attachment-homethumbs size-homethumbs wp-post-image" alt="" loading="lazy"></a>
+//  </div>
+//
+//  <div class="conteudoPost">
+//    <h1>
+//      <a href="https://spacetoday.com.br/atualizacoes-da-osiris-rex-guardando-as-amostras-do-asteroide-bennu-ao-vivo/">ATUALIZAÇÕES DA OSIRIS-REX – GUARDANDO AS AMOSTRAS DO ASTEROIDE BENNU | AO VIVO</a>
+//    </h1>
+//  </div>
+//
+//  <div class="authorPost">
+//    <p>Por: Space Today</p>
+//  <div>
+
+}
--- test/spacetoday_crawler_test.dart
+++ test/spacetoday_crawler_test.dart
@@ -1,11 +1,36 @@
-import 'package:spacetoday_crawler/spacetoday_crawler.dart';
+import 'package:spacetoday_crawler/src/core/model/post.dart';
+import 'package:spacetoday_crawler/src/core/parser/IHtmlParser.dart';
+import 'package:spacetoday_crawler/src/core/parser/post_parser.dart';
+import 'package:test/test.dart';
 
-// void main() {
-//   test('adds one to input values', () {
-//     final calculator = Calculator();
-//     expect(calculator.addOne(2), 3);
-//     expect(calculator.addOne(-7), -6);
-//     expect(calculator.addOne(0), 1);
-//     expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-//   });
-// }
+import 'test_data/category_page_data.dart';
+import 'test_data/empty_category_page_data.dart';
+import 'test_data/home_page_data.dart';
+
+void main() {
+  PostParser parser;
+  setUp(() {
+    parser = PostParser();
+  });
+
+  test('Testing success PostParser with HomePage', () {
+    List<Post> posts = parser.parse(homePage);
+
+    expect(posts != null, true);
+    expect(posts.isNotEmpty, true);
+    expect(posts.length == 12, true);
+  });
+
+  test('Testing Success PostParser with CategoryPage', () {
+    List<Post> posts = parser.parse(categoryPage);
+    expect(posts != null, true);
+    expect(posts.isNotEmpty, true);
+    expect(posts.length == 9, true);
+  });
+
+  test('Testing with empty category page', () {
+    List<Post> posts = parser.parse(emptyCategoryPage);
+    print(posts);
+    expect(posts.isEmpty, true);
+  });
+}
