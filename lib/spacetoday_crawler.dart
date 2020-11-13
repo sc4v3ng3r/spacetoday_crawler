48c6baf9a0ebd8b0a7c4a5233dc7fb0e092f3377
Boaventura <scavenger.gnu@gmail.com>
2020-11-13 17:52:37 -0300

    :rocket: exposing homepage model.

--- lib/spacetoday_crawler.dart
+++ lib/spacetoday_crawler.dart
@@ -10,3 +10,4 @@ export 'src/core/parser/IHtmlParser.dart' show IHtmlParser;
 export 'src/core/parser/post_parser.dart' show PostParser;
 export 'src/core/parser/post_content_parser.dart' show PostContentParser;
 export 'src/core/parser/search_results_parser.dart' show SearchResultsParser;
+export 'src/core/model/home_page.dart' show HomePage;
