b35571f523448594b0d852a446f901771e088197
Boaventura <scavenger.gnu@gmail.com>
2020-11-13 16:10:49 -0300

    :pushpin: exposing models and parsers.

--- lib/spacetoday_crawler.dart
+++ lib/spacetoday_crawler.dart
@@ -2,3 +2,11 @@ library spacetoday_crawler;
 
 export 'src/spacetoday_api.dart' show SpaceTodayApi;
 export 'src/core/model/exception.dart' show ParserException;
+export 'src/core/model/category.dart' show Category;
+export 'src/core/model/post.dart' show Post;
+export 'src/core/model/post_content.dart' show PostContent;
+export 'src/core/model/search_result.dart' show SearchResult;
+export 'src/core/parser/IHtmlParser.dart' show IHtmlParser;
+export 'src/core/parser/post_parser.dart' show PostParser;
+export 'src/core/parser/post_content_parser.dart' show PostContentParser;
+export 'src/core/parser/search_results_parser.dart' show SearchResultsParser;
